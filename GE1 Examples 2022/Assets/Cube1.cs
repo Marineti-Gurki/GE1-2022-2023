43683bb0e7f42020d453fc6cacf864a6295f0354
matissolus <matiss@olusedu.com>
2022-09-24 00:29:19 +0100

    Squashed commit of the following:
    commit 502fb8f609b9c4617b6386856e5e2d46b8831607
    Author: matissolus <matiss@olusedu.com>
    Date:   Sat Sep 24 00:28:06 2022 +0100
    
        Full circle of dodecahedrons
    
        - Colors not implemented (how do??)
        - Rotation not working
    
    commit 2fce710bba6f213e6217236704829150022bee0b
    Author: matissolus <matiss@olusedu.com>
    Date:   Sat Sep 24 00:17:48 2022 +0100
    
        Late Night Attempts
    
        Nearly working as intended?
    
    commit b884389e5b67a50a9f0bba1f589df6c27bbed434
    Author: matissolus <matiss@olusedu.com>
    Date:   Fri Sep 23 22:56:15 2022 +0100
    
        First Commit

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class Cube1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject myPrefab;
+    GameObject g;
+    public List<GameObject> listofGs;
     void Start()
     {
-        
+        float radius = 1;
+        for (int i = 0; i <= 10; i++)
+        {
+            int numbPrefabs = (int)(2.0f * Mathf.PI * i * radius);
+            float theta = Mathf.PI * 2.0f / ((float)numbPrefabs);
+            for (int j = 0; j < numbPrefabs; j++)
+            {
+                float angle = j * theta;
+                float x = (radius) * Mathf.Cos(angle) * i;
+                float y = (radius) * Mathf.Sin(angle) * i;
+                g = Instantiate(myPrefab, new Vector3(x, 0, y), Quaternion.identity);
+            }
+            listofGs.Add(g);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        // transform.Rotate(0.1f, 0, 0.1f);
+        for (int i = 0; i < listofGs.Count - 1; i++)
+        {
+            listofGs[i].transform.Rotate(0.1f, 0, 0.1f);
+        }
     }
 }
