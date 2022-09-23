2fce710bba6f213e6217236704829150022bee0b
matissolus <matiss@olusedu.com>
2022-09-24 00:17:48 +0100

    Late Night Attempts
    Nearly working as intended?

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class Cube1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject myPrefab;
+
     void Start()
     {
-        
+        for (int i = 0; i < 6; i++)
+        {
+            float radius = 1;
+            float xPos = Mathf.PI * radius * Mathf.Cos(i);
+            float yPos = Mathf.PI * radius * Mathf.Sin(i);
+            Instantiate(myPrefab, new Vector3(xPos, 0, yPos), Quaternion.identity);
+            for (int j = 0; j < i; j++)
+            {
+                float xPos2 = Mathf.PI * (radius * 3) * Mathf.Cos(j);
+                float yPos2 = Mathf.PI * (radius * 3) * Mathf.Sin(j);
+                Instantiate(myPrefab, new Vector3(xPos2, 0, yPos2), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        transform.Rotate(0.1f, 0, 0.1f);
     }
 }
