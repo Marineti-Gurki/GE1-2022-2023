dc35558ca0a0f45679cef9c30bc6c1cedaa4d84d
Marineti-Gurki <matiss2000@inbox.lv>
2022-09-30 18:01:37 +0100

    Rotate is fixed

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 public class Cube1 : MonoBehaviour
 {
     // Start is called before the first frame update
+<<<<<<< Updated upstream
     void Start()
     {
         
+=======
+    public GameObject myPrefab;
+    public List<GameObject> listofGs;
+    void Start()
+    {
+        
+        float radius = 2;
+        for (int i = 0; i <= 10; i++)
+        {
+            int numbPrefabs = (int)(2.0f * Mathf.PI * i * radius);
+            float theta = Mathf.PI * 2.0f / ((float)numbPrefabs);
+            for (int j = 0; j < numbPrefabs; j++)
+            {
+                float angle = j * theta;
+                float x = (radius) * Mathf.Cos(angle) * i;
+                float y = (radius) * Mathf.Sin(angle) * i;
+
+                
+                GameObject g = Instantiate(myPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                listofGs.Add(g);
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         transform.Translate(0, 0, 1);
+=======
+        // g.transform.Rotate(0.1f, 0, 0.1f);
+        // transform.Rotate(0.1f, 0, 0.1f);
+        for (int i = 0; i < listofGs.Count; i++)
+        {
+            listofGs[i].transform.Rotate(300*Time.deltaTime, 0, 300*Time.deltaTime);
+        }
+>>>>>>> Stashed changes
     }
 }
