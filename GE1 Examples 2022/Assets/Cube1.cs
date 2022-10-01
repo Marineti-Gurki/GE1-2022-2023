2eda839e346a8632548749fcf45492b62843238b
matissolus <matiss2000@inbox.lv>
2022-10-01 16:52:28 +0100

    Colours Added
    Rotation fixed, colours added

--- GE1 Examples 2022/Assets/Cube1.cs
+++ GE1 Examples 2022/Assets/Cube1.cs
@@ -6,11 +6,12 @@ public class Cube1 : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject myPrefab;
-    GameObject g;
     public List<GameObject> listofGs;
     void Start()
     {
         float radius = 1;
+        GameObject middle = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        listofGs.Add(middle);
         for (int i = 0; i <= 10; i++)
         {
             int numbPrefabs = (int)(2.0f * Mathf.PI * i * radius);
@@ -20,18 +21,17 @@ public class Cube1 : MonoBehaviour
                 float angle = j * theta;
                 float x = (radius) * Mathf.Cos(angle) * i;
                 float y = (radius) * Mathf.Sin(angle) * i;
-                g = Instantiate(myPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                GameObject g = Instantiate(myPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                g.GetComponent<Renderer>().material.color = Color.HSVToRGB((j / (float)numbPrefabs), 1, 1);
+                listofGs.Add(g);
             }
-            listofGs.Add(g);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        g.transform.Rotate(0.1f, 0, 0.1f);
-        transform.Rotate(0.1f, 0, 0.1f);
-        for (int i = 0; i < listofGs.Count - 1; i++)
+        for (int i = 0; i < listofGs.Count; i++)
         {
             listofGs[i].transform.Rotate(0.1f, 0, 0.1f);
         }
