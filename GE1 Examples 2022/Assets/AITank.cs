57ed18e7f981dc1aa67e5a9f4cf2264548d01b7a
matissolus <matiss2000@inbox.lv>
2022-10-14 18:29:29 +0100

    Lab Started

--- GE1 Examples 2022/Assets/AITank.cs
+++ GE1 Examples 2022/Assets/AITank.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class AITank : MonoBehaviour {
+public class AITank : MonoBehaviour
+{
 
     public float radius = 10;
     public int numWaypoints = 5;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player;
 
     public void OnDrawGizmos()
     {
@@ -20,20 +21,36 @@ public class AITank : MonoBehaviour {
             // Put code here to draw the gizmos
             // Use sin and cos to calculate the positions of the waypoints 
             // You can draw gizmos using
-            // Gizmos.color = Color.green;
-            // Gizmos.DrawWireSphere(pos, 1);
+
+            Gizmos.color = Color.green;
+            int rings = 3;
+            int somethingelse = 10;
+            float theta = Mathf.PI * 2.0f / (float)somethingelse;
+            for (int ring = 1; ring < rings; ring++)
+            {
+                int count = Mathf.RoundToInt(Mathf.PI * 2.0f * ring);
+                for (int i = 0; i < count; i++)
+                {
+                    Vector3 pos = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
+                    pos = transform.TransformPoint(pos);
+
+                    Gizmos.DrawWireSphere(pos, 1);
+                }
+            }
         }
     }
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
